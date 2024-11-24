f5eb66a0f1eecd03fe9a479bf6eba768dc5d80f1
aidenbohlander <aiden.bohlander@gmail.com>
2024-11-24 14:40:28 -0800

    basic terminal stuff

---
+++ pipe-bomb/terminaltext.gd
@@ -0,0 +1,19 @@
+extends RichTextLabel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		var key = event.as_text()
+		print(key)
+		if key == "Space":
+			key = " "
+		text += key
